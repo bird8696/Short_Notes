56d03c1801189e02c50822df385dd77ff1cf9c41
김태현 <kthkmj00@gmail.com>
2023-11-14 12:15:24 +0900

    3st

--- app/src/main/java/com/zypher/shortnotes/MainActivity.java
+++ app/src/main/java/com/zypher/shortnotes/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
             Log.d("MainActivity", "Button clicked");
             Intent intent = new Intent(MainActivity.this, NoteActivity.class);
             startActivity(intent);
+            finish();
+            return;
         });
     }
 }
